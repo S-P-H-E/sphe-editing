800da6014224552283385506845abfc1b063de5a
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-03-05 10:10:48 +0200

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '@/styles/globals.css'
+import Script from "next/script"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-T17856LBRY"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-T17856LBRY', {
+        page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
+      
+        <Component {...pageProps} />
+    </>
+  )
 }
