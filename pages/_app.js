108d696c7284f8a388f53289c809a56a6fab5771
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-03-07 07:56:43 +0200

    vercel analytics add

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
-import Script from "next/script"
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-T17856LBRY"/>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-        __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-T17856LBRY', {
-        page_path: window.location.pathname,
-        });
-        `,
-        }}
-      />
-      
-        <Component {...pageProps} />
+      <Component {...pageProps} />
+      <Analytics />
     </>
-  )
+  );
 }
